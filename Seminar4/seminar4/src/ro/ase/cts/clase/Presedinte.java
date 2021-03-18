ef9ce9cd166c6bff3be57a96c8a9ec7f729c9bde
angiex98 <angiex98@DESKTOP-ANGELICA>
2021-03-18 18:06:27 +0200

    Singleton

---
+++ Seminar4/seminar4/src/ro/ase/cts/clase/Presedinte.java
@@ -0,0 +1,62 @@
+package ro.ase.cts.clase;
+
+public class Presedinte {
+	
+	private String nume;
+	private int varsta;
+	private int mandat;
+	
+	public Presedinte(String nume, int varsta, int mandat) {
+		this.nume = nume;
+		this.varsta = varsta;
+		this.mandat = mandat;
+	}
+
+	public Presedinte() {
+		this.nume="Putin";
+		this.varsta=68;
+		this.mandat=4;
+	}
+	
+	private static Presedinte instance=new Presedinte(); // de obicei e private, dar la eager initialization putem lasa instanta publica
+	
+	public static Presedinte getInstance() {
+		return instance;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb=new StringBuilder("Presedinte{");
+		sb.append("nume=").append(nume).append(", varsta=")
+		.append(varsta).append(", mandat=").append(mandat);
+		sb.append("}");
+		return sb.toString();
+	}
+
+	public String getNume() {
+		return nume;
+	}
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+
+	public int getVarsta() {
+		return varsta;
+	}
+
+	public void setVarsta(int varsta) {
+		this.varsta = varsta;
+	}
+
+	public int getMandat() {
+		return mandat;
+	}
+
+	public void setMandat(int mandat) {
+		this.mandat = mandat;
+	}
+
+	
+	
+}
---
+++ Seminar4/seminar4/src/ro/ase/cts/clase/PresedinteLazy.java
@@ -0,0 +1,58 @@
+package ro.ase.cts.clase;
+
+public class PresedinteLazy {
+	private static String nume;
+	private static int varsta;
+	private static int mandat;
+	
+	private static PresedinteLazy instance=null;
+	
+	//lazy amana apelul constructorului
+	
+	public PresedinteLazy() {
+		this.nume="Presedinte";
+		this.varsta=40;
+		this.mandat=2;
+	}
+	public PresedinteLazy(String nume, int varsta, int mandat) {
+	
+		this.nume = nume;
+		this.varsta = varsta;
+		this.mandat = mandat;
+	}
+	
+	public static synchronized PresedinteLazy getInstance() {
+		if(instance ==null) {
+			instance=new PresedinteLazy(nume, varsta, mandat);
+		}
+		return instance;
+	}
+	
+	public String toString() {
+		final StringBuilder sb=new StringBuilder("Presedinte{");
+		sb.append("nume=").append(nume).append(", varsta=")
+		.append(varsta).append(", mandat=").append(mandat);
+		sb.append("}");
+		return sb.toString();
+	}
+	public String getNume() {
+		return nume;
+	}
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	public int getVarsta() {
+		return varsta;
+	}
+	public void setVarsta(int varsta) {
+		this.varsta = varsta;
+	}
+	public int getMandat() {
+		return mandat;
+	}
+	public void setMandat(int mandat) {
+		this.mandat = mandat;
+	}
+	
+	
+}
